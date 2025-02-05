890b79d190c6c5a894ecb1d5f4547fa320ed256a
Roger Westerbø <roger.westerbo@gmail.com>
2025-02-05 16:35:21 +0100

    Adding confirmdialog for state buttons

--- src/app/virtualmachines/components/virtualmachine-status/virtualmachine-status.component.ts
+++ src/app/virtualmachines/components/virtualmachine-status/virtualmachine-status.component.ts
@@ -1,13 +1,17 @@
 import { Resource } from '@rork8s/ror-resources/models';
-import { Component, Input } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
+import { Component, inject, Input } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { PowerComponent } from '../../../shared/components/power/power.component';
 import { PowerState } from '../../../core/models/powerstate';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmDialog, ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ConfirmationService } from 'primeng/api';
 
 @Component({
   selector: 'app-virtualmachine-status',
   standalone: true,
-  imports: [TranslateModule, PowerComponent],
+  imports: [TranslateModule, PowerComponent, ToastModule, ConfirmDialogModule],
+  providers: [ConfirmationService],
   templateUrl: './virtualmachine-status.component.html',
   styleUrl: './virtualmachine-status.component.scss',
 })
@@ -15,6 +19,10 @@ export class VirtualmachineStatusComponent {
   @Input() virtualmachine: Resource | undefined;
 
   powerstates = PowerState;
+  confirmDialogPosition = 'right';
+
+  private confirmationService = inject(ConfirmationService);
+  private translateService = inject(TranslateService);
 
   getPowerState(): PowerState {
     if (!this.virtualmachine) {
@@ -30,5 +38,55 @@ export class VirtualmachineStatusComponent {
       default:
         return PowerState.Unknown;
     }
+  }
+
+  turnOff() {
+    this.confirmationService.confirm({
+      message: 'Are you sure you want to turn off this virtual machine?',
+      header: 'Turn off?',
+      accept: () => {
+        // Turn off the virtual machine
+      },
+    });
+  }
+
+  turnOn() {
+    this.confirmationService.confirm({
+      message: 'Are you sure you want to turn on this virtual machine?',
+      header: 'Turn on?',
+      accept: () => {
+        // Turn on the virtual machine
+      },
+    });
+  }
+
+  restart() {
+    this.confirmationService.confirm({
+      message: 'Are you sure you want to restart this virtual machine?',
+      header: 'Restart?',
+      accept: () => {
+        // Restart the virtual machine
+      },
+    });
+  }
+
+  suspend() {
+    this.confirmationService.confirm({
+      message: 'Are you sure you want to restart this virtual machine?',
+      header: 'Restart?',
+      accept: () => {
+        // Suspend the virtual machine
+      },
+    });
+  }
+
+  delete() {
+    this.confirmationService.confirm({
+      message: 'Are you sure you want to restart this virtual machine?',
+      header: 'Restart?',
+      accept: () => {
+        // Delete the virtual machine
+      },
+    });
   }
 }
