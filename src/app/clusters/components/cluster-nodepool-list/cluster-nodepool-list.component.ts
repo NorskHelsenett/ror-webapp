f1e13eed27bf99fc8c1e729bb2751bc1a59d98cb
Roger Westerbø <roger.westerbo@gmail.com>
2025-05-22 12:52:48 +0200

    Supporting old and ny nodepools list

--- src/app/clusters/components/cluster-nodepool-list/cluster-nodepool-list.component.ts
+++ src/app/clusters/components/cluster-nodepool-list/cluster-nodepool-list.component.ts
@@ -1,60 +1,38 @@
-import { Resourcesv2Service } from './../../../core/services/resourcesv2.service';
-import { NodePool } from './../../../core/models/clusterOrder';
-import { AsyncPipe, JsonPipe, LowerCasePipe, TitleCasePipe } from '@angular/common';
+import { AsyncPipe, LowerCasePipe, TitleCasePipe } from '@angular/common';
 import { ConfigService } from './../../../core/services/config.service';
-import { ChangeDetectionStrategy, Component, inject, Input, OnDestroy, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Component, inject, Input, OnDestroy, OnInit, ChangeDetectorRef, Output, EventEmitter } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
-import { ClusterNodeListComponent } from '../cluster-node-list/cluster-node-list.component';
-import { ClusterNodepoolCreateorupdateComponent } from '../cluster-nodepool-createorupdate/cluster-nodepool-createorupdate.component';
-import { MessageService } from 'primeng/api';
 import { AclService } from '../../../core/services/acl.service';
 import { AclAccess, AclScopes } from '../../../core/models/acl-scopes';
-import { catchError, finalize, Observable, share, tap } from 'rxjs';
+import { catchError, Observable, share } from 'rxjs';
+import { NodePool } from '@rork8s/ror-resources/models';
+import { TrueFalseComponent } from '../../../shared/components/true-false/true-false.component';
 import { FormatBytesPipe } from '../../../shared/pipes';
-import { Resource, ResourceQuery, ResourceSet } from '@rork8s/ror-resources/models';
-import { SpinnerComponent } from '../../../shared/components/spinner/spinner.component';
 
 @Component({
   selector: 'app-cluster-nodepool-list',
-  imports: [
-    TranslateModule,
-    TableModule,
-    LowerCasePipe,
-    TitleCasePipe,
-    ButtonModule,
-    ClusterNodeListComponent,
-    ClusterNodepoolCreateorupdateComponent,
-    AsyncPipe,
-    FormatBytesPipe,
-    SpinnerComponent,
-    JsonPipe,
-  ],
+  imports: [TranslateModule, TableModule, LowerCasePipe, TitleCasePipe, ButtonModule, AsyncPipe, TrueFalseComponent, FormatBytesPipe],
   templateUrl: './cluster-nodepool-list.component.html',
   styleUrl: './cluster-nodepool-list.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ClusterNodepoolListComponent implements OnInit, OnDestroy {
   @Input() nodepools: any[] = [];
-  @Input() cluster: any;
+  @Input() editable: boolean = false;
+  @Output() onEdit = new EventEmitter<any>();
+  @Output() onDelete = new EventEmitter<any>();
   showNodepoolEditor: boolean = false;
   rows = 10;
   rowsPerPage: number[] = [10, 20, 50];
-  selectedNodepool: any | undefined;
+
   aclFetchError: any;
   adminOwner$: Observable<boolean> | undefined;
 
-  clusterResource: Resource | undefined;
-  clusterResourceSet$: Observable<ResourceSet> | undefined;
-  clusterResourceSetFetchError: any;
-  isLoading = true; // Start with loading state active
-
   private configService = inject(ConfigService);
-  private messageService = inject(MessageService);
+
   private aclService = inject(AclService);
   private changeDetector = inject(ChangeDetectorRef);
-  private resourcesv2Service = inject(Resourcesv2Service);
 
   ngOnInit(): void {
     this.rows = this.configService?.config?.rows;
@@ -68,80 +46,15 @@ export class ClusterNodepoolListComponent implements OnInit, OnDestroy {
         throw error;
       }),
     );
-
-    this.fetchKubernetesClusterResource();
   }
 
   ngOnDestroy(): void {}
 
-  toggleNodepoolEditor(nodePool?: any): void {
-    this.selectedNodepool = nodePool;
-    this.showNodepoolEditor = !this.showNodepoolEditor;
-    this.changeDetector.detectChanges();
+  editNodepool(nodePool?: any): void {
+    this.onEdit.emit(nodePool);
   }
 
-  deleteNodepool(nodePool: any): void {
-    this.messageService.add({ severity: 'success', summary: 'Nodepool deleted', detail: 'Nodepool deleted successfully' });
-
-    const newlist = this.cluster?.topology?.nodePools.filter((np: any) => np?.name !== nodePool?.name);
-    this.cluster.topology.nodePools = newlist;
-  }
-
-  fetchKubernetesClusterResource(): void {
-    this.isLoading = true;
-    this.clusterResourceSetFetchError = null;
-    this.clusterResource = undefined;
-    this.changeDetector.detectChanges(); // Force update to show loading state
-    console.log('Datacenter name:', this.cluster?.workspace?.datacenter?.name);
-    console.log('Cluster name:', this.cluster?.clusterName);
-    const query: ResourceQuery = {
-      versionkind: {
-        Group: '',
-        Kind: 'KubernetesCluster',
-        Version: 'general.ror.internal/v1alpha1',
-      },
-      filters: [
-        {
-          field: 'metadata.name',
-          type: 'string',
-          operator: 'eq',
-          value: this.cluster?.clusterName,
-        },
-        {
-          field: 'rormeta.ownerref.subject',
-          type: 'string',
-          operator: 'eq',
-          value: this.cluster?.workspace?.datacenter?.name,
-        },
-        {
-          field: 'rormeta.ownerref.scope',
-          type: 'string',
-          operator: 'eq',
-          value: 'datacenter',
-        },
-      ],
-    };
-    console.log('Query:', query);
-    this.clusterResourceSet$ = this.resourcesv2Service.getResources(query).pipe(
-      tap((data: ResourceSet) => {
-        if (data?.resources?.length === 1) {
-          this.clusterResource = data?.resources[0];
-          return this.clusterResource;
-        } else {
-          this.clusterResource = undefined;
-          return null;
-        }
-      }),
-      catchError((error: any) => {
-        this.clusterResourceSetFetchError = error;
-        this.isLoading = false;
-        this.changeDetector.detectChanges(); // Force update on error
-        throw error;
-      }),
-      finalize(() => {
-        this.isLoading = false;
-        this.changeDetector.detectChanges();
-      }),
-    );
+  deleteNodepool(nodePool?: any): void {
+    this.onDelete.emit(nodePool);
   }
 }
--- src/app/clusters/pages/cluster-nodepools/cluster-nodepools.component.ts
+++ src/app/clusters/pages/cluster-nodepools/cluster-nodepools.component.ts
@@ -1,25 +1,50 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, Input, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, Input, OnDestroy, OnInit } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
-import { ClusterService } from '../../services';
 import { PriceService } from '../../../core/services/price.service';
-import { Subscription } from 'rxjs';
+import { catchError, finalize, Observable, Subscription, tap } from 'rxjs';
 import { Price } from '../../../core/models/price';
 import { ClusterNodepoolListComponent } from '../../components/cluster-nodepool-list/cluster-nodepool-list.component';
 
+import { ClusterNodepoolCreateorupdateComponent } from '../../components/cluster-nodepool-createorupdate/cluster-nodepool-createorupdate.component';
+import { SpinnerComponent } from '../../../shared/components';
+import { AsyncPipe, JsonPipe } from '@angular/common';
+import { Resourcesv2Service } from '../../../core/services/resourcesv2.service';
+import { MessageService } from 'primeng/api';
+import { Resource, ResourceQuery, ResourceSet } from '@rork8s/ror-resources/models';
+import { LucideAngularModule, AsteriskIcon } from 'lucide-angular';
+
 @Component({
   selector: 'app-cluster-nodepools',
-  imports: [TranslateModule, ClusterNodepoolListComponent],
+  imports: [
+    TranslateModule,
+    ClusterNodepoolListComponent,
+    ClusterNodepoolCreateorupdateComponent,
+    SpinnerComponent,
+    AsyncPipe,
+    LucideAngularModule,
+    JsonPipe,
+  ],
   templateUrl: './cluster-nodepools.component.html',
   styleUrl: './cluster-nodepools.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ClusterNodepoolsComponent implements OnInit, OnDestroy {
+  readonly asteriskIcon = AsteriskIcon;
   @Input() cluster: any;
   prices: Price[];
+  showNodepoolEditor: boolean = false;
+  selectedNodepool: any | undefined;
+  nodepools: any[] = [];
 
-  private clusterService = inject(ClusterService);
+  clusterResource: Resource | undefined;
+  clusterResourceSet$: Observable<ResourceSet> | undefined;
+  clusterResourceSetFetchError: any;
+  isLoading = true;
+  loadedFromResources = false;
+
   private priceService = inject(PriceService);
   private changeDetector = inject(ChangeDetectorRef);
+  private resourcesv2Service = inject(Resourcesv2Service);
+  private messageService = inject(MessageService);
 
   private subscriptions = new Subscription();
 
@@ -30,13 +55,130 @@ export class ClusterNodepoolsComponent implements OnInit, OnDestroy {
         this.changeDetector.detectChanges();
       }),
     );
+
+    this.fetchKubernetesClusterResource();
   }
 
   ngOnDestroy(): void {
     this.subscriptions.unsubscribe();
   }
 
-  getCluster(): any {
-    return this.clusterService.getCluster();
+  toggleNodepoolEditor(nodePool?: any): void {
+    this.selectedNodepool = nodePool;
+    this.showNodepoolEditor = !this.showNodepoolEditor;
+    this.changeDetector.detectChanges();
+  }
+
+  fetchKubernetesClusterResource(): void {
+    this.isLoading = true;
+    this.clusterResourceSetFetchError = null;
+    this.clusterResource = undefined;
+    this.loadedFromResources = false;
+    this.changeDetector.detectChanges();
+
+    const query: ResourceQuery = {
+      versionkind: {
+        Group: '',
+        Kind: 'KubernetesCluster',
+        Version: 'general.ror.internal/v1alpha1',
+      },
+      filters: [
+        {
+          field: 'metadata.name',
+          type: 'string',
+          operator: 'eq',
+          value: this.cluster?.clusterName,
+        },
+        {
+          field: 'rormeta.ownerref.subject',
+          type: 'string',
+          operator: 'eq',
+          value: this.cluster?.workspace?.datacenter?.name,
+        },
+        {
+          field: 'rormeta.ownerref.scope',
+          type: 'string',
+          operator: 'eq',
+          value: 'datacenter',
+        },
+      ],
+    };
+    this.clusterResourceSet$ = this.resourcesv2Service.getResources(query).pipe(
+      tap((data: ResourceSet) => {
+        if (data?.resources?.length === 1) {
+          this.clusterResource = data?.resources[0];
+          this.loadedFromResources = true;
+          return this.clusterResource;
+        } else {
+          this.clusterResource = undefined;
+          this.nodepools = undefined;
+          return null;
+        }
+      }),
+      catchError((error: any) => {
+        this.clusterResourceSetFetchError = error;
+        this.isLoading = false;
+        this.changeDetector.detectChanges();
+        throw error;
+      }),
+      finalize(() => {
+        this.setNodepools(this.clusterResource);
+        this.isLoading = false;
+        this.changeDetector.detectChanges();
+      }),
+    );
+  }
+
+  editNodepool(nodePool?: any): void {
+    this.selectedNodepool = nodePool;
+    this.showNodepoolEditor = true;
+    this.changeDetector.detectChanges();
+  }
+
+  deleteNodepool(nodePool: any): void {
+    this.messageService.add({ severity: 'success', summary: 'Nodepool deleted', detail: 'Nodepool deleted successfully' }); // translate
+
+    // const newlist = this.cluster?.topology?.nodePools.filter((np: any) => np?.name !== nodePool?.name);
+    // this.cluster.topology.nodePools = newlist;
+    // implement
+  }
+
+  setNodepools(clusterResource: Resource): void {
+    this.nodepools = clusterResource?.kubernetescluster?.spec?.topology?.workers.nodePools;
+    if (!this.nodepools) {
+      this.nodepools = [];
+    }
+
+    for (const nodepool of this.nodepools) {
+      const metrics = this.cluster?.topology?.nodePools?.find((np: any) => np?.name === nodepool?.name)?.metrics;
+      if (metrics) {
+        nodepool.metrics = metrics;
+      }
+    }
+
+    if (this.nodepools.length === 0) {
+      for (const clusterPool of this.cluster?.topology?.nodePools) {
+        this.nodepools.push({
+          machineClass: clusterPool?.machineClass,
+          provider: clusterPool?.workspace?.datacenter?.provider,
+          name: clusterPool?.name,
+          replicas: clusterPool?.metrics?.nodeCount,
+          autoscaling: {
+            enabled: false,
+            minReplicas: 0,
+            maxReplicas: 0,
+            scalingRules: null,
+          },
+          metadata: {
+            labels: null,
+            annotations: null,
+          },
+          taints: null,
+          metrics: clusterPool?.metrics,
+        });
+      }
+    }
+
+    this.changeDetector.detectChanges();
   }
 }
