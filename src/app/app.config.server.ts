ef86097ba9c2db866f26f1661dad9f1448d378ae
Roger Westerbø <roger.westerbo@gmail.com>
2025-08-22 15:44:07 +0200

    Adding routes to ssr

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,9 +1,10 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { appConfig } from './app.config';
-import { provideServerRendering } from '@angular/ssr';
+import { provideServerRendering, withRoutes } from '@angular/ssr';
+import { serverRoutes } from './app.routes.server';
 
 const serverConfig: ApplicationConfig = {
-  providers: [provideServerRendering()],
+  providers: [provideServerRendering(withRoutes(serverRoutes))],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
